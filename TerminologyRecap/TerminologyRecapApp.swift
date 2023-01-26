187a8fdc6c9227da9ffa01231a0d25e21308997f
Russell Gordon <rgordon@lcs.on.ca>
2023-01-26 09:43:09 -0500

    Removed ContentView and set up project with new file: SymbolView.

--- TerminologyRecap/TerminologyRecapApp.swift
+++ TerminologyRecap/TerminologyRecapApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TerminologyRecapApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SymbolView()
         }
     }
 }
---
+++ TerminologyRecap/Views/SymbolView.swift
@@ -0,0 +1,20 @@
+//
+//  SymbolView.swift
+//  TerminologyRecap
+//
+//  Created by Russell Gordon on 2023-01-26.
+//
+
+import SwiftUI
+
+struct SymbolView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SymbolView_Previews: PreviewProvider {
+    static var previews: some View {
+        SymbolView()
+    }
+}
