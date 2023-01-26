7519536a226bf1c46637599f4770509b84a9ac7a
Russell Gordon <rgordon@lcs.on.ca>
2023-01-26 09:36:09 -0500

    Initial Commit

---
+++ TerminologyRecap/TerminologyRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminologyRecapApp.swift
+//  TerminologyRecap
+//
+//  Created by Russell Gordon on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TerminologyRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
